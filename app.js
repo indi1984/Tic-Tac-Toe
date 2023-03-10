4b1c5b256aefac96fe36ecbece190d69665015bf
Kevin Harp <kharp1984@gmail.com>
2023-03-10 18:43:42 -0500

    moved order of functions and spelling checks

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-//* Declerations
+//* Decelerations
 const info = document.querySelector('#info');
 const currentPlayer = document.querySelector('#player');
 const gameBoard = document.querySelector('#gameBoard');
 const startButton = document.querySelector('#startButton');
 const tdCells = document.querySelectorAll('td');
-
 
 
 //* Initialize ticTacToeBoard object
@@ -29,6 +28,89 @@ let ticTacToeBoard = {
 };
 
 
+//* Assigning Event Listeners to the buttons and gameBoard
+startButton.addEventListener('click', startingPlayer);
+resetButton.addEventListener('click', reset);
+gameBoard.addEventListener('click', getCoords);
+
+
+//* Upon clicking the Start Game button, a random starting player (X or O)
+//* is chosen based on the Math.random function.   The ticTacToe.player value
+//* is assigned and the player span element is set to display the current player
+function startingPlayer() {
+let starter = Math.floor(Math.random() * 2);
+  if (starter === 1) {
+    ticTacToeBoard.player = 'X';
+    currentPlayer.innerHTML = ticTacToeBoard.player;
+  } else {
+    ticTacToeBoard.player = 'O';
+    currentPlayer.innerHTML = ticTacToeBoard.player;
+  };
+  console.log(ticTacToeBoard.player);  //! CONSOLE LOG
+};
+
+
+//* Gets the coordinates of the gameBoard cell where the current player clicked.
+//* Picked string coords are then converted to a numeric array and passed to the 
+//* assignCoords function.   The clicked cell variable is also passed to the 
+//* assignedCoords function
+function getCoords(event) {  
+const cell = event.target;
+  if (cell.tagName === "TD") {
+    let strCoords = cell.dataset.coords.split(',');
+    const coordinates = strCoords.map((elem) => parseInt(elem));
+    //ticTacToeBoard.selectedCell = event.target;  //TODO Do not need
+    assignCoords(coordinates, cell);
+  };
+  cell.classList.add('disabled');
+};
+
+
+//* Checks the current player and if cell is free then pushes the gameBoard cell
+//* value pair to the current players ticTacToeBoard X or O Array.  Then calls
+//* the playerMove function to switch players
+function assignCoords(coordinates, cell) {
+  let isDisabled = cell.classList.contains('disabled');
+  if (!isDisabled) {
+    if (ticTacToeBoard.player === 'X' && ticTacToeBoard.player != '') {
+      ticTacToeBoard.playerXArray.push(coordinates);
+    } else if (ticTacToeBoard.player === 'O' && ticTacToeBoard.player != '') {
+      ticTacToeBoard.playerOArray.push(coordinates);
+    }
+    playerMove(cell);
+  };
+};
+  
+
+//* Changes the player span element to the Current Player (X or O),
+//* flips the value in ticTacToeBoard.player, and sets the gameBoard cell
+//* to the current players' value
+function playerMove(cell) {
+  if (ticTacToeBoard.player === "X") {
+    cell.innerHTML = "X";
+    currentPlayer.innerHTML = "O";
+    ticTacToeBoard.player = "O";
+  } else if (ticTacToeBoard.player === "O") {
+    cell.innerHTML = "O";
+    currentPlayer.innerHTML = "X";
+    ticTacToeBoard.player = "X";
+  };
+};
+
+
+//* Resets the game to the default loaded state
+function reset() {
+  for (let i = 0; i < tdCells.length; i++) {
+    tdCells[i].innerHTML = '';
+    tdCells[i].classList.remove('disabled');
+  };
+  currentPlayer.innerHTML = "PRESS START GAME BUTTON";
+  ticTacToeBoard.player = null;
+  ticTacToeBoard.selectedCell = null;
+  ticTacToeBoard.playerOArray = [];
+  ticTacToeBoard.playerXArray = [];
+};
+
 
 //! WIP function to check for winning rows based on ticTacToeBoard.winningArray
 function checkRows() {
@@ -49,94 +131,4 @@ function checkColumns() {
       console.log('PLAYER Y WINS!');
     };
   };
-};
-
-
-
-//* Assigning Event Listeners to the buttons and gameBoard
-startButton.addEventListener('click', startingPlayer);
-resetButton.addEventListener('click', reset);
-gameBoard.addEventListener('click', getCoords);
-
-
-
-//* Upon clicking the Start Game button, a random starting player (X or O)
-//* is chosen based on the Math.random function.   The ticTacToe.player value
-//* is assigned and the player span element is set to display the current player
-function startingPlayer() {
-let starter = Math.floor(Math.random() * 2);
-  if (starter === 1) {
-    ticTacToeBoard.player = 'X';
-    currentPlayer.innerHTML = ticTacToeBoard.player;
-  } else {
-    ticTacToeBoard.player = 'O';
-    currentPlayer.innerHTML = ticTacToeBoard.player;
-  };
-  console.log(ticTacToeBoard.player);  //! CONSOLE LOG
-};
-
-
-
-//* Gets the coordinates of the gameBoard cell where the current player clicked.
-//* Picked string coords are then converted to a numeric array and passed to the 
-//* assignCoords function.   The clicked cell variable is also passed to the 
-//* assignedCoords function
-function getCoords(event) {  
-const cell = event.target;
-  if (cell.tagName === "TD") {
-    let strCoords = cell.dataset.coords.split(',');
-    const coordinates = strCoords.map((elem) => parseInt(elem));
-    //ticTacToeBoard.selectedCell = event.target;  //TODO Do not need
-    assignCoords(coordinates, cell);
-  };
-  cell.classList.add('disabled');
-};
-
-
-
-//* Checks the current player and if cell is free then pushes the gameBoard cell
-//* value pair to the current players ticTacToeBoard X or O Array.  Then calls
-//* the playerMove function to switch players
-function assignCoords(coordinates, cell) {
-  let isDisabled = cell.classList.contains('disabled');
-  if (!isDisabled) {
-    if (ticTacToeBoard.player === 'X' && ticTacToeBoard.player != '') {
-      ticTacToeBoard.playerXArray.push(coordinates);
-    } else if (ticTacToeBoard.player === 'O' && ticTacToeBoard.player != '') {
-      ticTacToeBoard.playerOArray.push(coordinates);
-    }
-    playerMove(cell);
-  };
-};
-  
-
-
-//* Changes the player span element to the Current Player (X or O),
-//* flips the value in ticTacToeBoard.player, and sets the gameBoard cell
-//* to the current players' value
-function playerMove(cell) {
-  if (ticTacToeBoard.player === "X") {
-    cell.innerHTML = "X";
-    currentPlayer.innerHTML = "O";
-    ticTacToeBoard.player = "O";
-  } else if (ticTacToeBoard.player === "O") {
-    cell.innerHTML = "O";
-    currentPlayer.innerHTML = "X";
-    ticTacToeBoard.player = "X";
-  };
-};
-
-
-
-//* Resets the game to the default loaded state
-function reset() {
-  for (let i = 0; i < tdCells.length; i++) {
-    tdCells[i].innerHTML = '';
-    tdCells[i].classList.remove('disabled');
-  };
-  currentPlayer.innerHTML = "PRESS START GAME BUTTON";
-  ticTacToeBoard.player = null;
-  ticTacToeBoard.selectedCell = null;
-  ticTacToeBoard.playerOArray = [];
-  ticTacToeBoard.playerXArray = [];
 };
