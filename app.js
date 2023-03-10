f8955fc9869e0249222ef1bd8c08d5aa34bfdd7e
Kevin Harp <kharp1984@gmail.com>
2023-03-10 17:33:46 -0500

    before changing cell innerHTML

--- app.js
+++ app.js
@@ -1,63 +1,93 @@
 const info = document.querySelector('#info');
-const currentPlayer = document.querySelector('#playerStatus');
+const currentPlayer = document.querySelector('#player');
 const gameBoard = document.querySelector('#gameBoard');
 const startButton = document.querySelector('#startButton');
 
 
 let ticTacToeBoard = {
-  player: '',
-  selectedCell: '',
+  player: null,
+  selectedCell: null,  //TODO Do not need
   playerXArray: [],
-  playerYArray: [],
-  winningArray: [],
-}
+  playerOArray: [],
+  winningArray: [
+    // Rows
+    [[0,0], [0,1], [0,2]],
+    [[1,0], [1,1], [1,2]],
+    [[2,0], [2,1], [2,2]],
+    //Columns
+    [[0,0], [1,0], [2,0]],
+    [[0,1], [1,1], [2,1]],
+    [[0,2], [1,2], [2,2]],
+    //Diags
+    [[0,0], [1,1], [2,2]],
+    [[0,2], [1,1], [2,0]],
+  ],
+};
+
+
+
+
+function checkRows() {
+  for (let row = 0; row < ticTacToeBoard.length - 1; row++) {
+   if (ticTacToeBoard.playerXArray === ticTacToeBoard.winningArray[row]) {
+      console.log('PLAYER X WINS!');
+   } else if (ticTacToeBoard.playerYArray === ticTacToeBoard.winningArray[row]) {
+      console.log('PLAYER Y WINS!');
+   }
+  }
+};
 
 
 startButton.addEventListener('click', startingPlayer);
 gameBoard.addEventListener('click', getCoords);
 
 
-
 function startingPlayer() {
 let starter = Math.floor(Math.random() * 2);
   if (starter === 1) {
     ticTacToeBoard.player = 'X';
+    currentPlayer.innerHTML = ticTacToeBoard.player;
   } else {
     ticTacToeBoard.player = 'O';
+    currentPlayer.innerHTML = ticTacToeBoard.player;
   };
-  console.log(ticTacToeBoard.player);
-  return ticTacToeBoard.player;
+  console.log(ticTacToeBoard.player);  //! CONSOLE LOG
 };
 
 
-function getCoords(event) {
-  const cell = event.target;
-  if (cell.tagName === "TD") {
-    let strCoords = cell.dataset.coords.split(',');
-    const coordinates = strCoords.map((elem) => parseInt(elem));
-    ticTacToeBoard.selectedCell = event.target;
-    assignCoords(coordinates);
+function getCoords(event) {  
+const cell = event.target;
+  console.log(cell);
+  if (cell != "O"  & cell != "X") {
+    if (cell.tagName === "TD") {
+      let strCoords = cell.dataset.coords.split(',');
+      const coordinates = strCoords.map((elem) => parseInt(elem));
+      ticTacToeBoard.selectedCell = event.target;
+      console.log(ticTacToeBoard.selectedCell);  //TODO Do not need
+      assignCoords(coordinates, cell);
+    }
   };
-  return coordinates;
-
 };
 
 
-function assignCoords(coordinates, event) {
-  cell = event.target;
-  if (ticTacToeBoard.player === 'X') {
+function assignCoords(coordinates, cell) {
+  if (ticTacToeBoard.player === 'X' && ticTacToeBoard.player != '') {
     ticTacToeBoard.playerXArray.push(coordinates);
-    cell.innerHTML = 'X';
-  } else {
-    ticTacToeBoard.playerYArray.push(coordinates);
-    cell.innerHTML = 'O';
+  } else if (ticTacToeBoard.player === 'O' && ticTacToeBoard.player != '') {
+    ticTacToeBoard.playerOArray.push(coordinates);
   };
-  return coordinates;
+  playerMove(cell);
 };
 
 
-function playerMove() {
+function playerMove(cell) {
   if (ticTacToeBoard.player === "X") {
-    
-  }
+    cell.innerHTML = "X";
+    currentPlayer.innerHTML = "O";
+    ticTacToeBoard.player = "O";
+  } else if (ticTacToeBoard.player === "O") {
+    currentPlayer.innerHTML = "X";
+    cell.innerHTML = "O";
+    ticTacToeBoard.player = "X";
+  };
 };
